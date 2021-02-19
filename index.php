ae4f91e517d832468f2a6907c0f4feb296b2fafc
zenka4 <zenka4@inbox.lt>
2021-02-19 09:37:24 +0200

    create 3 files with test data

---
+++ index.php
@@ -0,0 +1,14 @@
+<!-- Commitai turi tureti prasmingus komentarus.
+Kodas turi būti rašomas laikantis PSR-1 ir PSR-2.
+Būtina taikyti SOLID, nepamirštam DRY bei KISS.
+Privaloma komentuoti kodą (minimum bent blokais) bei privaloma rašyti PHP Doc. -->
+
+<!-- Skaitymas iš failo:
+
+Reikia parašyti mini programėlę, kuri gali nuskaityti duomenys iš failo į asociatyvinį masyvą.
+Duomenys gali būti pateikti vienu iš šių formatu: CSV, XML, JSON.
+Programa turi būti parašyta taip, kad pridėjus naują (ar pašalinus esamą) failo formatą nereiktų nieko koreguoti esamame kode (išskyrus konfiguraciją). 
+
+Kaip tai daugmaž turėtų veikti:
+Perduodame programai failo pavadinimą.
+Ekrane pamatome failo turinį. -->
